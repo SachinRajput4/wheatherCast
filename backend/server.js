b0a1657c8d37c2351ccfbb8282e67277668fd765
Sachin Rajput <sachinrajput988724@gmail.com>
2025-10-04 18:15:01 +0530

    small error fixing while hosting render

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,6 @@ import { connectDB } from "./config/db.js";
 //import  Routes
 import user2Router from "./routes/user2Route.js";
 import user1Router from "./routes/user1Route.js";
-import CollectionRouter from "./routes/collectionRoute.js";
-import inviteStudentRouter from "./routes/inviteStudentLinkRoute.js";
-import viewAttendanceHistoryRouter from "./routes/viewAttendanceHistoryRoute.js";
-import awsSearchRouter from "./routes/awsSearchRoute.js";
-import collectionItemControllerRouter from "./routes/collectionItemRoute.js" 
-import analyticsRouter from "./routes/analyticsRoute.js"
 
 
 // Import weather routes
@@ -44,12 +38,7 @@ connectDB();
 // Routes
 app.use("/api/user2",user2Router);
 app.use("/api/user1",user1Router);
-app.use("/api/collection", CollectionRouter); 
-app.use('/api/invite', inviteStudentRouter);
-app.use('/api/view-attendance-history', viewAttendanceHistoryRouter);
-app.use('/api/aws-search', awsSearchRouter);
-app.use('/api/collection-item', collectionItemControllerRouter);
-app.use('/api/analytics', analyticsRouter);
+
 
 // Use weather routes
 app.use('/api/weather', weatherRouter);
